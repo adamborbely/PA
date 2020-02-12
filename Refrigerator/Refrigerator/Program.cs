b56f6f17311ebac14c15a17b301c361d1f8be729
Adam <borbelya90@gmail.com>
2020-02-12 14:11:29 +0100

    Starting of the process

---
+++ Refrigerator/Refrigerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Refrigerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
